9863912f69129a29d9cfb94fd1d77fff5583de4e
안로빈 <anrobin719@gmail.com>
2019-09-12 20:49:39 +0900

    [feat] Create react app for trobot project

---
+++ trobot-frontend/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import classes from './App.module.scss';
+
+class App extends Component {
+  render() {
+    return (
+      <div className={classes.App}>
+        <div className={classes.inbox}>hello trobot v2</div>
+      </div>
+    );
+  }
+}
+
+export default App;
